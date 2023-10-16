6ed6e717a8b33d8aa21ed1135b00357804d75228
DaniDelHoyo <danieldelhoyog@gmail.com>
2023-10-16 13:19:35 +0200

    Tests for receptor and system preparation

--- openmm/tests/test_openmm.py
+++ openmm/tests/test_openmm.py
@@ -24,3 +24,58 @@
 # *
 # **************************************************************************
 
+import os
+
+from pyworkflow.tests import BaseTest, setupTestProject, DataSet
+from pwem.protocols import ProtImportPdb
+
+from ..protocols import ProtOpenMMReceptorPrep, ProtOpenMMSystemPrep
+
+class TestOpenMMPrepareReceptor(BaseTest):
+  @classmethod
+  def setUpClass(cls):
+    cls.ds = DataSet.getDataSet('model_building_tutorial')
+    setupTestProject(cls)
+    cls._runImportPDB()
+    cls._waitOutput(cls.protImportPDB, 'outputPdb', sleepTime=5)
+
+  @classmethod
+  def _runImportPDB(cls):
+    cls.protImportPDB = cls.newProtocol(
+      ProtImportPdb,
+      inputPdbData=1,
+      pdbFile=cls.ds.getFile('PDBx_mmCIF/1ake_mut1.pdb'))
+    cls.proj.launchProtocol(cls.protImportPDB, wait=False)
+
+  @classmethod
+  def _runPrepareReceptor(cls):
+    protPrepare = cls.newProtocol(
+      ProtOpenMMReceptorPrep,
+      inputAtomStruct=cls.protImportPDB.outputPdb,
+      addRes=True)
+
+    cls.launchProtocol(protPrepare)
+    return protPrepare
+
+  def test(self):
+    protPrepare = self._runPrepareReceptor()
+    self._waitOutput(protPrepare, 'outputStructure', sleepTime=10)
+    self.assertIsNotNone(getattr(protPrepare, 'outputStructure', None))
+
+
+class TestOpenMMPrepareSystem(TestOpenMMPrepareReceptor):
+    @classmethod
+    def _runPrepareSystem(cls, protPrepare):
+        protPrepare = cls.newProtocol(
+            ProtOpenMMSystemPrep,
+            inputStructure=protPrepare.outputStructure)
+
+        cls.launchProtocol(protPrepare)
+        return protPrepare
+
+    def test(self):
+        protPrepareRec = self._runPrepareReceptor()
+        self._waitOutput(protPrepareRec, 'outputStructure', sleepTime=10)
+        protPrepare = self._runPrepareSystem(protPrepareRec)
+        self._waitOutput(protPrepare, 'outputSystem', sleepTime=10)
+        self.assertIsNotNone(getattr(protPrepare, 'outputSystem', None))
