d8175f69e11d59b06a3542c8cbf754f06321e1c8
DaniDelHoyo <danieldelhoyog@gmail.com>
2025-05-29 12:28:10 +0200

    Receptor preparation superseeded by the one in pwchem

--- openmm/protocols/__init__.py
+++ openmm/protocols/__init__.py
@@ -4,7 +4,6 @@
 # Find documentation here: https://scipion-em.github.io/docs/docs/developer/creating-a-protocol
 # **************************************************************************
 
-from .protocol_receptor_prep import ProtOpenMMReceptorPrep
 from .protocol_system_prep import ProtOpenMMSystemPrep
 from .protocol_system_simulation import ProtOpenMMSystemSimulation
 from .protocol_openduck_simulation import ProtOpenDuckSimulation
