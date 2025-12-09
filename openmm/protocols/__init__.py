b854f7ed07df44120bd59096017a94b005333f88
Blanca Pueche <blancapueche@gmail.com>
2025-12-09 10:38:53 +0100

    delete unused files

--- openmm/protocols/__init__.py
+++ openmm/protocols/__init__.py
@@ -9,4 +9,3 @@ from .protocol_system_simulation import ProtOpenMMSystemSimulation
 from .protocol_openduck_simulation import ProtOpenDuckSimulation
 from .protocol_interaction_energy import ProtOpenMMInteractionEnergy
 from .protocol_constantph_simulation import ProtOpenMMSystemSimulationConstantPH
-from .protocol_constantph_system_prep import ProtOpenMMSystemPrepConstantPH
