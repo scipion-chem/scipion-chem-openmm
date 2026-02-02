f55a7a10f05dec72181578b88a20a3ca1efde542
Blanca Pueche <blancapueche@gmail.com>
2026-02-02 12:56:46 +0100

    reduced step size

--- openmm/tests/test_openmm.py
+++ openmm/tests/test_openmm.py
@@ -78,7 +78,7 @@ class TestOpenMMSimulation(TestOpenMMPrepareSystem):
   def _runSimulation(cls, protPrepareS):
       protSim = cls.newProtocol(
         ProtOpenMMSystemSimulation,
-        inputSystem=protPrepareS.outputSystem,
+        inputSystem=protPrepareS.outputSystem, stepSize=0.002,
         maxIter=100, nSteps=100, nTraj=10)
 
       cls.launchProtocol(protSim)
